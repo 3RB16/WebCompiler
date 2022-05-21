f655a3dcbbb7e863daf46680f343a8636243befb
muhammad arbi <73764445+3RB16@users.noreply.github.com>
2022-05-21 07:48:15 +0200

    adding parser output

--- Models/ScannerModel.cs
+++ Models/ScannerModel.cs
@@ -1,4 +1,4 @@
-﻿using WebCompiler.Models;
+using WebCompiler.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,18 +10,30 @@ namespace WebCompiler.Models
 {
     public class ScannerModel
     {
-        static public List<string> run(List <string> data) {
+        static public List<string> run(List <string> data , bool type) {
             List<string> answer = new List<string>();
             int LineNumber = 1;
             BuildScannerModel scanner = new BuildScannerModel();
+            List<string> parserList = new List<string>();
+            int currentErrors = 0;
             bool isComment = false;
             foreach(string line in data)
             {
                 scanner.TakeData(line, LineNumber , ref answer , ref isComment);
+                // parser handelling
+                if(currentErrors != scanner.numberOfErrors)
+                {
+                    parserList.Add("Line : " + LineNumber + " Not Matched\n");
+                    currentErrors = scanner.numberOfErrors;
+                } else
+                {
+                    parserList.Add("Line : " + LineNumber + " Matched" + "    " + "Rule used : " + scanner.getRole(line)  + "\n");
+                }
                 ++LineNumber;
             }
             answer.Add("Total NO of errors: " + scanner.numberOfErrors);
-            return answer;
+            parserList.Add("Total NO of errors: " + scanner.numberOfErrors);
+            return type ? parserList : answer;
         }
     }
 }
